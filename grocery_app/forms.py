c8b9e76df0f45d5937a03f29cc4d0ee82abf9a93
xchbx <ceina.ellison@gmail.com>
2025-05-02 13:55:21 -0400

    updated wtforms_sqlalchemy

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, SelectField, SubmitField
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
 
 class GroceryStoreForm(FlaskForm):
