ef1409efa22cf9e24ea117bb007796e51eea164a
xchbx <ceina.ellison@gmail.com>
2025-05-08 17:33:18 -0400

    fixed error & modify routes to add flask_login.current_user object

--- grocery_app/main/routes.py
+++ grocery_app/main/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
 from grocery_app.models import GroceryStore, GroceryItem
 from grocery_app.forms import GroceryStoreForm, GroceryItemForm
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 # Import from grocery_app package so that we can run app
 from grocery_app.extensions import db
@@ -26,7 +26,8 @@ def new_store():
     if form.validate_on_submit(): 
         new_grocery_store = GroceryStore(
             title=form.title.data,
-            address=form.address.data
+            address=form.address.data,
+            created_by=current_user
         )
         db.session.add(new_grocery_store)
         db.session.commit()
@@ -34,7 +35,7 @@ def new_store():
         flash("New Grocery Store was created successfully.")
         return redirect(url_for("main.store_detail", store_id=new_grocery_store.id))
     
-    return render_template('new_store.html', form=form)
+    return render_template('main/new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
 @login_required
@@ -47,15 +48,16 @@ def new_item():
             price=form.price.data,
             category=form.category.data,
             photo_url=form.photo_url.data,
-            store=form.store.data
+            store=form.store.data,
+            created_by=current_user
         )
         db.session.add(new_grocery_item)
         db.session.commit()
-
+        
         flash("New Grocery Item was created successfully.")
         return redirect(url_for("main.item_detail", item_id=new_grocery_item.id))
 
-    return render_template('new_item.html', form=form)
+    return render_template('main/new_item.html', form=form)
 
 @main.route('/store/<store_id>', methods=['GET', 'POST'])
 @login_required
@@ -94,5 +96,5 @@ def item_detail(item_id):
         flash("Item details updated successfully.")
         return redirect(url_for("main.item_detail", item_id=item.id))
 
-    return render_template('item_detail.html', item=item, form=form)
+    return render_template('main/item_detail.html', item=item, form=form)
 
--- grocery_app/models.py
+++ grocery_app/models.py
@@ -1,5 +1,6 @@
 from grocery_app.extensions import db
 from grocery_app.utils import FormEnum
+from flask_login import UserMixin
 
 
 class ItemCategory(FormEnum):
@@ -35,7 +36,7 @@ class GroceryItem(db.Model):
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_by = db.relationship('User', backref='items')
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     """User model"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
